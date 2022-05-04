68c9d0303ae15f1a8703b088922aa4e620a9eba8
nitinjunejaa <kumar.juneja11@gmail.com>
2022-05-05 05:14:08 +0530

    File Updated

--- Structure.cpp
+++ Structure.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int main () {
 // Structure is a Data type consisting of multiple entities with different data types.
   
-// typedef struct Student
-// {
-//     int UID;
-//     char FavChar;
-//     float cgpa;
-// } ep;
+typedef struct Student
+{
+    int UID;
+    char FavChar;
+    float cgpa;
+} ep;
+
+// Struct body-->
+
+    ep nitin;
+    nitin.UID = 76;
+    nitin.FavChar = 'j';
+    nitin.cgpa = 8.39;
+    cout<<"The Value of cgpa is "<<nitin.cgpa<<endl;
+    cout<<"The Value of FavChar is "<<nitin.FavChar<<endl;
+    cout<<"The Value of UID is "<<nitin.UID<<endl;
 
 // Unioun
 // Union is used for when you hav defined 3 differt entities for same data type but want to use just 1 at a time,
@@ -21,38 +32,21 @@ union money // These here are just different forms of money defined, judt for re
     float rupees;
     char car;
 };
-
-// {
-//     int UID;
-//     char FavChar;
-//     float cgpa;
-// } ep;
-
-int main() {
 // Unioun body -->
 
     union money m1;
     m1.rice = 65;
     m1.car = 'c'; 
     cout<<m1.car<<endl;
-    
-// Struct body-->
 
-    //ep nitin;
-    // nitin.UID = 76;
-    // nitin.FavChar = 'j';
-    // nitin.cgpa = 8.39;
-    // cout<<"The Value of cgpa is "<<nitin.cgpa<<endl;
-    // cout<<"The Value of FavChar is "<<nitin.FavChar<<endl;
-    // cout<<"The Value of UID is "<<nitin.UID<<endl;
 
 // Enum -->
     enum Languages {Cpp, java, Python, Kotlin, React, node_js};
     Languages L1 = Python;
     cout<<(L1==2)<<endl;
-    // cout<<Cpp<<endl;
-    // cout<<java<<endl;
-    // cout<<React<<endl;
-    // cout<<Kotlin<<endl;
+    cout<<Cpp<<endl;
+    cout<<java<<endl;
+    cout<<React<<endl;
+    cout<<Kotlin<<endl;
     return 0;
 }
